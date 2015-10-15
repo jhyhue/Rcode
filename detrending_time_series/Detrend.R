694285ecb198c6dc90bc468f23312ca4dc58ce46
swl <weezel.van@gmail.com>
2015-10-15 15:59:56 +0100

    Tidying

--- detrending_time_series/Detrend.R
+++ detrending_time_series/Detrend.R
@@ -1,85 +1,73 @@
-#==============================================================================
-# Detrending time-series data using penalised splines
-#==============================================================================
+#### Detrending time-series data #####
+## Method using penalised splines
+## See for information on method:
+## https://ideas.repec.org/p/got/gotcrc/131.html
+## http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.372.6892
 
 ## Libraries
 library(mgcv)
 library(reshape)
 
-#======================================
-#### Trending data #### 
-# Quarterly GDP data 1959-2009 
-# (Stock & Watson, 2010) 
-#======================================
+## Start with trending data example
+##  Quarterly GDP data 1959-2009 from Stock & Watson (2010)
+dat<-scan("GDP.txt");plot(dat)
+gdp<-ts(dat,frequency=4,start=c(1959,1))
 
-## Load data and set parameters
-data<-scan("GDP.txt")
-
-n=length(data)
+## Set parameters
+n=length(dat)
 t=1:n
 k=floor(n/3) # Number of knots
 
 ## Fit data using AR1 structure
-data.gamm=gamm(data~s(t,k=k,bs="cs"),correlation=corAR1())
+d.gamm=gamm(dat~s(t,k=k,bs="cs"),correlation=corAR1())
 
-# Create time series
-gdp<-ts(data,frequency=4,start=c(1959,1))
-gdp.trend<-ts(fitted(data.gamm$lme),frequency=4,start=c(1959,1))
+## Subtract trend from time-series
+gdp.trend<-ts(fitted(d.gamm$lme),frequency=4,start=c(1959,1))
 gdp.dt<-ts(gdp-gdp.trend,frequency=4,start=c(1959,1)) # Detrended data
-
-# Plot results
-par(mfrow=c(2,1),mar=c(4,5,2,2),las=1,tck=.02,bty="n",family="serif")
-
-# Upper panel: original data
-plot(gdp,xlab="",ylab="GDP",type="p",pch=1,cex=.75,axes=FALSE)
-lines(gdp.trend,col="red",lwd=3)
-axis(1,tick=F)
-axis(2,tick=F, las=2)
-
-# Lower panel: detrended data
-plot(gdp.dt,type="l",xlab="",ylab="Detrended values",axes=FALSE)
-axis(1,tick=F)
-axis(2,tick=F, las=2)
-
-#======================================
-#### Seasonal data #### 
-# Monthly temperature  data 
-# Guatemala 1901-2013 (CRU) 
-#======================================
-
-## Load data
-gtm<-read.csv("TemperatureGuatemala_1901-2013.csv",header=TRUE,sep=",")
-
-## Reshape data to long
-d<-melt(gtm,id="YEAR")
-colnames(d)<-c("year","month","temperature")
-d<-d[order(d$year,d$month),]
-
-data<-d$temperature
-
-## Set parameters
-n=length(data)
-t=1:n
-k=floor(n/3) # Number of knots
-
-## Fit data using AR1 structure (Can take a while)
-data.gamm=gamm(data~s(t,k=k,bs="cs"),correlation=corAR1())
-
-## Create time series
-temp<-ts(data,freq=12,start=c(1901,1))
-temp.trend<-ts(fitted(data.gamm$lme),frequency=12,start=c(1901,1))
-temp.dt<-ts(temp-temp.trend,frequency=12,start=c(1901,1)) # Detrended data
 
 ## Plot results
 par(mfrow=c(2,1),mar=c(4,5,2,2),las=1,tck=.02,bty="n",family="serif")
 
 # Upper panel: original data
-plot(temp,xlab="",ylab="Temperature",type="p",pch=1,cex=.75,axes=FALSE)
-lines(temp.trend,col="red",lwd=3)
-axis(1,tick=F)
+plot(gdp,xlab="",ylab="GDP",type="p",pch=1,cex=.75,axes=FALSE)
+lines(gdp.trend,col="red",lwd=3)
 axis(2,tick=F, las=2)
 
-# Lower pannel: detrended data
-plot(temp.dt,type="l",xlab="",ylab="Monthly anomalies",axes=FALSE)
-axis(1,tick=F)
+# Lower panel: detrended data
+plot(gdp.dt,type="l",xlab="",ylab="Detrended values",axes=FALSE)
+axis(1,tick=F); axis(2,tick=F, las=2)
+
+## Example with seasonal data 
+## Monthly temperature data for Guatemala 1901-2013 (CRU) 
+gtm<-read.csv("GTMTemp1901-2013.csv",header=TRUE,sep=",")
+tmp<-ts(data,freq=12,start=c(1901,1))
+
+## Reshape data to long
+d<-melt(gtm,id="YEAR")
+colnames(d)<-c("year","month","temperature")
+d<-d[order(d$year,d$month),]
+dat<-d$temperature
+
+## Set parameters
+n=length(dat)
+t=1:n
+k=floor(n/3) # Number of knots
+
+## Fit data using AR1 structure (Can take a while)
+d.gamm=gamm(dat~s(t,k=k,bs="cs"),correlation=corAR1())
+
+## Subtract trend from time-series
+# This basically creates anomalies
+tmp.trend<-ts(fitted(d.gamm$lme),frequency=12,start=c(1901,1))
+tmp.dt<-ts(tmp-tmp.trend,frequency=12,start=c(1901,1))
+
+## Plot results
+par(mfrow=c(2,1),mar=c(4,5,2,2),las=1,tck=.02,bty="n",family="serif")
+# Upper panel: original data
+plot(tmp,xlab="",ylab="Temperature",type="p",pch=1,cex=.75,axes=FALSE)
+lines(tmp.trend,col="red",lwd=3)
 axis(2,tick=F, las=2)
+
+# Lower panel: detrended data
+plot(tmp.dt,type="l",xlab="",ylab="Monthly anomalies",axes=FALSE)
+axis(1,tick=F);axis(2,tick=F, las=2)
